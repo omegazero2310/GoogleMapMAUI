482480e4f3df328ca0bc2595631f27d07609cd28
Nguyễn Văn An <annv3@bagroup.vn>
2022-11-11 14:43:11 +0700

    demo polyline

--- MAUIDotNet7/MainPage.xaml.cs
+++ MAUIDotNet7/MainPage.xaml.cs
@@ -8,6 +8,20 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var polyline = new Polyline();
+        polyline.Positions.Add(new Position(40.77d, -73.93d));
+        polyline.Positions.Add(new Position(40.81d, -73.91d));
+        polyline.Positions.Add(new Position(40.83d, -73.87d));
 
+        polyline.IsClickable = true;
+        polyline.StrokeColor = Color.FromRgb(0, 0, 255);
+        polyline.StrokeWidth = 5f;
+        polyline.Tag = "POLYLINE";
+
+        map.Polylines.Add(polyline);
+        map.MoveCamera(new CameraUpdate(new Position(40.77d, -73.93d)));
+    }
 }
 
